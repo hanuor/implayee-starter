574a58b8b3afc312b68641f581b4bd4383c935ed
hanuor <hanuortech@gmail.com>
2017-12-09 17:47:07 +0530

    Fixed code

--- app/src/main/java/com/raywenderlich/implayee/VideoActivity.kt
+++ app/src/main/java/com/raywenderlich/implayee/VideoActivity.kt
@@ -2,20 +2,13 @@ package com.raywenderlich.implayee
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.Button
 
 
 class VideoActivity : AppCompatActivity() {
 
-    private var playPauseButton: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_video)
-
-        playPauseButton = findViewById(R.id.playPauseButton)
-
-        playPauseButton?.setOnClickListener({
-        })
 
     }
 
